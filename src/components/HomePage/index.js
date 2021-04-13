f4940e561414973e41ffcfb31bf10ab25961d0c7
dale.sakamoto <daletsakamoto@gmail.com>
2021-04-13 13:02:58 -0700

    matching form completed

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -19,6 +19,7 @@ import './HomePage.css'
 
 function HomePage() {
   const [rating, setRating] = useState(50)
+  const [sendYesButton, setSendYesButton] = useState("false");
   // const inputEl = useRef(null);
   // const onButtonClick = () => {
   //   // `current` points to the mounted text input element
@@ -27,27 +28,27 @@ function HomePage() {
 
   return (
     <div className='Homepage-container'>
-      <Container>
-  <Row>
-    <Col xs={6} md={4}>
-    </Col>
-    <Col xs={12} md={8}>
-      <Card style={{ width: '20rem' }}>
-        <Card.Img variant="top" src="./background_1.jpeg" />
-        <Card.Body>
-          <Card.Title>Make a Match</Card.Title>
-          <Card.Text>
-            Answer a few quick questions and get connected today:
-          </Card.Text>
-        </Card.Body>
-        <Card.Body >
-          <QuestionsCarousel />
-        </Card.Body>
-      </Card>
-      <Slider min={0} max={100} onChange={(e) => setRating(e)} value={rating} />
-    </Col>
-  </Row>
-  </Container>
+      <Container className={sendYesButton ? null : "slit-out-vertical"} >
+        <Row>
+          <Col xs={6} md={4}>
+          </Col>
+          <Col xs={12} md={8}>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img variant="top" src="./background_1.jpeg" />
+              <Card.Body>
+                <Card.Title>Make a Match</Card.Title>
+                <Card.Text>
+                  Answer a few quick questions and get connected today:
+                </Card.Text>
+              </Card.Body>
+              <Card.Body >
+                <QuestionsCarousel sendYesButton={sendYesButton} setSendYesButton={setSendYesButton}/>
+              </Card.Body>
+            </Card>
+            {/* <Slider min={0} max={100} onChange={(e) => setRating(e)} value={rating} /> */}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/QuestionsCarousel/index.js
+++ src/components/QuestionsCarousel/index.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import Slider, { createSliderWithTooltip } from 'rc-slider';
 
 import {
   ButtonGroup,
-  ToggleButton
+  ToggleButton,
+  Button
 } from 'react-bootstrap'
 
 import './QuestionsCarousel.css'
 
 import AutofillBox from '../AutofillBox'
 
-function QuestionsCarousel() {
+function QuestionsCarousel({sendYesButton, setSendYesButton}) {
   const Range = createSliderWithTooltip(Slider.Range);
-  const [salary, setSalary] = useState(0)
+  const [range, setRange] = useState([0, 0])
+
   const [radioValue, setRadioValue] = useState('1');
 
   const radios = [
@@ -22,6 +24,14 @@ function QuestionsCarousel() {
     { name: 'Mid (3-5 yrs)', value: '2' },
     { name: 'Sr (5+ yrs)', value: '3' },
   ];
+
+  const calcSalary = (val) => {
+    setRange(val)
+  }
+
+  const sendInfo = () => {
+    setSendYesButton(!sendYesButton)
+  }
 
 
   return (
@@ -48,10 +58,30 @@ function QuestionsCarousel() {
       </ButtonGroup>
       </div>
         <div className='Questions-Carousel-Slides'>
-          <Range min={0} max={200000} />
-        </div>
+        <Range
+          dotStyle={{ display: 'none' }}
+          step={null}
+          tipProps={{ visible: true }}
+          tipFormatter={value => {
+            if (value < 10000) return '$0'
+            else if (value >= 10000 && value < 100000) return `$${value}`.slice(0, 3) + 'k'
+            else return `$${value}`.slice(0, 4) + 'k'
+          }
+          }
+          min={0}
+          max={200000}
+          marks={{ 0: '$0', 10000: '', 20000: '', 30000: '', 40000: '', 50000: '$50k', 60000: '', 70000: '', 80000: '', 90000: '', 100000: '$100k', 110000: '', 120000: '', 130000: '', 140000: '', 150000: '$150k', 160000: '', 170000: '', 180000: '', 190000: '', 200000: '$200k' }}
+          defaultValue={range}
+          onChange={(val) => calcSalary(val)} />
+      </div>
+      <div className='Questions-Carousel-Slides'>
+        <p>Are you ready to be matched with you perfect company?</p>
+        <Button onClick={sendInfo} variant="primary" size="lg" active>
+          Yes!
+        </Button>
+      </div>
     </Carousel>
   );  
 }
 
-export default QuestionsCarousel
+export default QuestionsCarousel;

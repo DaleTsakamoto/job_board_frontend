80c8d72f82c86be6c562b1d096d3827c694079a2
dale.sakamoto <daletsakamoto@gmail.com>
2021-04-14 08:57:27 -0700

    signup and login forms created

--- src/components/CreateAccountModal/index.js
+++ src/components/CreateAccountModal/index.js
@@ -1,10 +1,12 @@
 import {
   Modal,
   Button,
-  Form
+  Form,
+  Row,
+  Col,
 } from 'react-bootstrap'
 
-function LoginModal(props) {
+function CreateAccountModal(props) {
   return (
     <Modal
     {...props}
@@ -14,21 +16,29 @@ function LoginModal(props) {
   >
     <Modal.Header closeButton>
       <Modal.Title id="contained-modal-title-vcenter">
-        Log In
+        Sign Up
       </Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <Form>
+        <Form>
+          <Form.Group>
+          <Row>
+            <Col>
+              <Form.Control placeholder="First name" />
+            </Col>
+            <Col>
+              <Form.Control placeholder="Last name" />
+            </Col>
+            </Row>
+            </Form.Group>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-            <Form.Text className="text-muted">
-              Your match is just around the corner.
-            </Form.Text>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicPasswordConfirm">
+          <Form.Control type="password" placeholder="Confirm Password" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
@@ -39,4 +49,4 @@ function LoginModal(props) {
   )
 }
 
-export default LoginModal
+export default CreateAccountModal
--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -3,16 +3,23 @@ import {
   Button
 } from 'react-bootstrap'
 
-
+import CreateAccountModal from '../CreateAccountModal'
 import './SplashPage.css'
 
 function SplashPage() {
+  const [modalShow, setModalShow] = useState(false);
 
   return (
-    <div className='SplashPage-Container'>
-      <h1>Search Right</h1>
-      <Button size="lg">Create Account</Button>
-    </div>
+    <>
+      <div className='SplashPage-Container'>
+        <h1>Search Right</h1>
+        <Button size="lg" onClick={() => setModalShow(true)}>Create Account</Button>
+      </div>
+      <CreateAccountModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 }
 

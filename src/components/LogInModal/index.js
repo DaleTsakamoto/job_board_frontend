d869d65d672584894397e071328202a265ac3a93
dale.sakamoto <daletsakamoto@gmail.com>
2021-04-14 10:57:24 -0700

    adding state to save login info

--- src/components/LogInModal/index.js
+++ src/components/LogInModal/index.js
@@ -1,3 +1,5 @@
+import {useState, useEffect} from 'react'
+
 import {
   Modal,
   Button,
@@ -5,6 +7,25 @@ import {
 } from 'react-bootstrap'
 
 function LoginModal(props) {
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+
+  useEffect(() => {
+    if (validated) {
+      console.log("PUT BACKEDN INFO HERE!")
+    }
+  },[validated])
+
   return (
     <Modal
     {...props}
@@ -18,22 +39,40 @@ function LoginModal(props) {
       </Modal.Title>
     </Modal.Header>
     <Modal.Body>
-      <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              required
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid password.
+              </Form.Control.Feedback>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             <Form.Text className="text-muted">
               Your match is just around the corner.
             </Form.Text>
-        </Form.Group>
+          </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
-      </Form>
+        </Form>
     </Modal.Body>
   </Modal>
   )
